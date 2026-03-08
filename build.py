e42b19fdd1e4f38c6b4df575a30d09d845161fa9
Arcadiy Ivanov <arcadiy@ivanov.biz>
2026-03-07 22:44:06 -0500

    Bump dev version

--- build.py
+++ build.py
@@ -23,7 +23,7 @@ use_plugin("python.coveralls")
 use_plugin("python.distutils")
 
 name = "karellen-rr-mcp"
-version = "0.2.2"
+version = "0.2.3.dev"
 
 summary = "MCP Server for rr Reverse Debugging"
 authors = [Author("Karellen, Inc.", "supervisor@karellen.co")]
